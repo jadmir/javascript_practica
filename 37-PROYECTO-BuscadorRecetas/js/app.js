2d521dbfe23557ff80d70028f86345430b075b54
Jadmir <jadmir1111@gmail.com>
2024-03-12 12:25:40 -0500

    recetas

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
   selectCateogiras.addEventListener('change', seleccionarCategoria)
 
   const resultado = document.querySelector('#resultado')
+  const modal = new bootstrap.Modal('#modal', {})
 
   obtenerCategorias()
 
@@ -71,6 +72,12 @@ function iniciarApp() {
       const recetaButton = document.createElement('BUTTON')
       recetaButton.classList.add('btn', 'btn-danger', 'w-100')
       recetaButton.textContent = 'Ver Receta'
+      //recetaButton.dataset.bsTarget = "#modal"
+      //recetaButton.dataset.bsToggle = "modal"
+      recetaButton.onclick = function() {
+        seleccionarReceta(idMeal)
+      }
+
 
       //inyectar en el html
       recetaCardBody.appendChild(recetaHeading)
@@ -83,6 +90,50 @@ function iniciarApp() {
 
       resultado.appendChild(recetaContenedor)
     });
+  }
+
+  function seleccionarReceta(id) {
+    const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => mostrarRecetaModal(resultado.meals[0]))
+  }
+
+  function mostrarRecetaModal(receta){
+    const {idMeal, strInstructions, strMeal, strMealThumb } = receta
+
+    //añadir contenido al modal
+    const modalTitle = document.querySelector('.modal .modal-title')
+    const modalBody = document.querySelector('.modal .modal-body')
+
+    modalTitle.textContent = strMeal
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+      <h3 class="my-3">Instrucciones</h3>
+      <p>${strInstructions}</p>
+      <h3 class="my-3">Ingredientes y Cantidades</h3>
+    `
+
+    const listGroup = document.createElement('UL')
+    listGroup.classList.add('list-group')
+    //mostrar cantidades e ingredientes
+    for(let i = 1; i <= 20; i++){
+      if(receta[`strIngredient${i}`]){
+        const ingrediente = receta[`strIngredient${i}`]
+        const cantidad = receta[`strMeasure${i}`]
+
+        const ingredientesLi = document.createElement('LI')
+        ingredientesLi.classList.add('list-group-item')
+        ingredientesLi.textContent = `${ingrediente} - ${cantidad}`
+
+        listGroup.appendChild(ingredientesLi)
+      }
+    }
+
+    modalBody.appendChild(listGroup)
+
+    //muestra el modal
+    modal.show()
   }
 
   function limpiarHTML(selector) {
