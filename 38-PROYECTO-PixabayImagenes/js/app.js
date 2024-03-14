aba6fb049280654cc7d56ced44feff8fa30ff469
Jadmir <jadmir1111@gmail.com>
2024-03-14 12:51:26 -0500

    finalizado

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,5 +1,11 @@
 const resultado = document.querySelector('#resultado')
 const formulario = document.querySelector('#formulario')
+const paginacionDiv = document.querySelector('#paginacion')
+
+const registrosPorPagina = 40
+let totalPaginas
+let iterador
+let paginaActual = 1
 
 window.onload = () => {
   formulario.addEventListener('submit', validarFormulario)
@@ -15,7 +21,7 @@ function validarFormulario(e) {
     return
   }
 
-  buscarImagenes(terminoBusqueda)
+  buscarImagenes()
 }
 
 function mostrarAlerta(mensaje) {
@@ -41,17 +47,90 @@ function mostrarAlerta(mensaje) {
   
 }
 
-function buscarImagenes(termino) {
+function buscarImagenes() {
+  const termino = document.querySelector('#termino').value
+
   const key = '42859084-1167136fa57e44e3389728cf5'
-  const url = `https://pixabay.com/api/?key=${key}&q=${termino}`
+  const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`
 
   fetch(url)
     .then(respuesta => respuesta.json())
     .then(resultado => {
+      totalPaginas = calcularPagina(resultado.totalHits)
       mostrarImagenes(resultado.hits)
     })
 }
 
+//generador que va a registar la cantidad de elementos de acuerdo a las paginas
+function *crearPaginador(total) {
+  for(let i = 1; i <= total; i++ ) {
+    yield i
+  }
+}
+
+function calcularPagina(total) {
+  return parseInt(Math.ceil(total/registrosPorPagina))
+}
+
 function mostrarImagenes(imagenes) {
-  console.log(imagenes)
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild)
+  }
+
+  //Iterar sobre el arreglo de imagenes y construir el html
+  imagenes.forEach(imagen => {
+    const { previewURL, likes, views, largeImageURL } = imagen
+
+    resultado.innerHTML += `
+      <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+        <div class="bg-white">
+          <img class="w-full" src="${previewURL}" >
+
+          <div class="p-4">
+            <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
+            <p class="font-bold">${views}<span class="font-light"> Veces Vista</span></p>
+
+            <a 
+              class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" 
+              href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+            >
+              Ver Imagen
+            </a>
+          </div>
+        </div>
+      </div>
+    `
+  });
+
+  //limipar el paginador previo
+  while(paginacionDiv.firstChild){
+    paginacionDiv.removeChild(paginacionDiv.firstChild)
+  }
+
+  //generar el nuevo html
+  imprimirPaginador()
+
+}
+
+function imprimirPaginador() {
+  iterador = crearPaginador(totalPaginas)
+
+  while(true) {
+    const {value, done } = iterador.next()
+    if(done) return
+
+    //en caso contrario genera un boton por cada elemento en el generador
+    const boton = document.createElement('a')
+    boton.href ='#'
+    boton.dataset.pagina = value
+    boton.textContent = value
+    boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded')
+
+    boton.onclick = () => {
+      paginaActual = value
+      buscarImagenes()
+    }
+
+    paginacionDiv.appendChild(boton)
+  }
 }
