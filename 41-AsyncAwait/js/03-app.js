c88ac1fbf767a41cbc7942311572433c9ef9228f
JADMIR IGNACIO VICENTE SIFUENTES <jvicentes@utp.edu.pe>
2024-03-26 21:13:58 -0500

    terminados

---
+++ 41-AsyncAwait/js/03-app.js
@@ -0,0 +1,26 @@
+function descargandoClientes() {
+    return new Promise((resolve, reject) => {
+        const error = true
+
+        setTimeout(() => {
+            if(!error) {
+                resolve('El listado de clientes se descargo correctamente')
+            } else {
+                reject('Error en la conexion')
+            }
+        }, 3000);
+    })
+}
+
+//Async Await
+const ejecutar = async () => {
+    try {
+        const respuesta = await descargandoClientes()
+
+        console.log(2+2)
+        console.log(respuesta)
+    } catch (error) {
+        console.log(error)
+    }
+}
+ejecutar()
---
+++ 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -0,0 +1,59 @@
+import { obtenerCliente, editarCliente } from './API.js'
+import { mostrarAlerta, validar } from './funciones.js'
+
+(function() {
+
+    //campos del formulario
+    const nombreInput = document.querySelector('#nombre')
+    const emailInput = document.querySelector('#email')
+    const empresaInput = document.querySelector('#empresa')
+    const telefonoInput = document.querySelector('#telefono')
+    const idInput = document.querySelector('#id')
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        const parametrosURL = new URLSearchParams(window.location.search)
+
+        const idCLiente = parseInt( parametrosURL.get('id'))
+
+        const cliente = await obtenerCliente(idCLiente)
+
+        mostrarCliente(cliente)
+
+        //submit al formulario
+        const formulario = document.querySelector('#formulario')
+        formulario.addEventListener('submit', validarCliente )
+    })
+
+    function mostrarCliente(cliente) {
+        const { nombre, empresa, email, telefono, id} = cliente
+
+        nombreInput.value = nombre
+        emailInput.value = email
+        empresaInput.value = empresa
+        telefonoInput.value = telefono
+        idInput.value = id
+    }
+
+    function validarCliente(e) {
+        e.preventDefault()
+
+        const cliente = {
+            nombre: nombreInput.value, 
+            email: emailInput.value, 
+            telefono: telefonoInput.value, 
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        console.log(cliente)
+
+        if(validar(cliente)) {
+            //mensaje de error
+            mostrarAlerta('Todos los campos son obligatorios')
+            return
+        }
+
+        //rescribe el objeto
+        editarCliente(cliente)
+    }
+})()
